117f80c45daf2b0f83988a9f3278b7ace7f89f9b
Jonghwi Kim <54308894+bellhwi@users.noreply.github.com>
2024-01-19 11:20:28 -0800

    remove map border

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -7,7 +7,7 @@ const Map = () => {
         loading='lazy'
         allowfullscreen
         src='https://www.google.com/maps/embed/v1/place?q=place_id:ChIJycWP7-Pn3IARTiG7pd2l22I&key=AIzaSyAsYP4vnD1mFgX4wWBRRd6CT0O1_hPhg6s'
-        className='w-full border-none'
+        className='w-full border-0'
       ></iframe>
     </div>
   )
