b4a425453b8bda53278e4a7fbf74385f723c4b44
Jonghwi Kim <54308894+bellhwi@users.noreply.github.com>
2024-05-15 22:31:44 -0700

    fix build error

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,42 +6,40 @@ import TopBtn from '@/components/TopBtn'
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='en' className='scroll-smooth'>
-      <body className='relative'>
-        <nav className='hidden absolute left-0 top-0 right-0 text-white z-30 lg:block'>
-          <Image
-            src='/img/navbar-bg.webp'
-            priority
-            width={5709}
-            height={321}
-            className='absolute inset-0 h-12 w-full'
-            alt='navigation bar background'
-          />
+    <div className='relative'>
+      <nav className='hidden absolute left-0 top-0 right-0 text-white z-30 lg:block'>
+        <Image
+          src='/img/navbar-bg.webp'
+          priority
+          width={5709}
+          height={321}
+          className='absolute inset-0 h-12 w-full'
+          alt='navigation bar background'
+        />
 
-          <div className='w-2/12 flex justify-between items-center relative mt-2.5'>
-            <Link href={`/`} className='ml-auto pr-4'>
-              <Logo footer />
-            </Link>
+        <div className='w-2/12 flex justify-between items-center relative mt-2.5'>
+          <Link href={`/`} className='ml-auto pr-4'>
+            <Logo footer />
+          </Link>
+        </div>
+      </nav>
+      {children}
+
+      {/* Mobile Call Button  */}
+      <a href='tel:+19493419100' className='lg:hidden'>
+        <div className='z-20 fixed right-0 bottom-0 m-4'>
+          <div className='flex items-center justify-center rounded-full bg-primary text-white w-12 h-12'>
+            <Image
+              src='/icons/phone.svg'
+              width={24}
+              height={24}
+              alt='phone icon'
+            />
           </div>
-        </nav>
-        {children}
+        </div>
+      </a>
 
-        {/* Mobile Call Button  */}
-        <a href='tel:+19493419100' className='lg:hidden'>
-          <div className='z-20 fixed right-0 bottom-0 m-4'>
-            <div className='flex items-center justify-center rounded-full bg-primary text-white w-12 h-12'>
-              <Image
-                src='/icons/phone.svg'
-                width={24}
-                height={24}
-                alt='phone icon'
-              />
-            </div>
-          </div>
-        </a>
-
-        <TopBtn />
-      </body>
-    </html>
+      <TopBtn />
+    </div>
   )
 }
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -2,9 +2,7 @@
 import Image from 'next/image'
 import Footer from '@/components/Footer'
 import Navbar from '@/components/Navbar'
-import { useState } from 'react'
 import 'photoswipe/dist/photoswipe.css'
-
 import { Gallery, Item } from 'react-photoswipe-gallery'
 
 export default function Portfolio({ setOpenNav, setOpenQuote }) {
